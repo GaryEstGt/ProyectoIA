b3d06e578b74e687269b8c6cb6147719f0441b07
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2020-05-03 11:03:57 -0600

    creación de proyecto Java y creación de clase pelicula

---
+++ ProyectoIA/src/main/java/com/mycompany/proyectoia/Pelicula.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyectoia;
+
+/**
+ *
+ * @author garya
+ */
+public class Pelicula {
+ private String titulo;
+ private String director;
+ private String color;
+ private String actorPrincipal;
+ private String lenguaje;
+ private String pais;
+ private String contenido;
+ private String año;
+ private String genero;
+ private double score;
+}
